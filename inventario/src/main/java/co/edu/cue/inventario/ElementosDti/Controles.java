c1b37b8c975c82e33f321389fd1e9bca701224d3
JuanPeñuela <juan.penuela@ingesoftsi.com>
2024-12-01 23:55:51 -0500

    test: se hacen las pruebas de integracion

--- inventario/src/main/java/co/edu/cue/inventario/ElementosDti/Controles.java
+++ inventario/src/main/java/co/edu/cue/inventario/ElementosDti/Controles.java
@@ -3,10 +3,12 @@ package co.edu.cue.inventario.ElementosDti;
 import co.edu.cue.inventario.Enums.EstadosElementos;
 import co.edu.cue.inventario.Enums.TipoDeElementos;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Document(collection = "controles")
+@Component
 public class Controles extends ElementosDti{
 
     public Controles(String identificacion, String nombre, String descripcion, TipoDeElementos tipo,
